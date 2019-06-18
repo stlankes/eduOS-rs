6d847a8b984370e3a30fa185764127b02a0c9631
Leonard Rapp <leonard.rapp@rwth-aachen.de>
2019-06-18 22:13:51 +0200

    aarch64/serial: use rust-aarch64 lib

--- src/arch/aarch64/serial.rs
+++ src/arch/aarch64/serial.rs
@@ -1,7 +1,7 @@
 use core::fmt;
 use spin::Mutex;
 use core::ptr;
-// use aarch64::io::*;
+use aarch64::io::*;
 
 /// A COM serial port.
 pub struct ComPort {
@@ -14,18 +14,6 @@ impl ComPort {
 	/// Create a new COM port with the specified base address.
 	pub const fn new(port_address: u32) -> Self {
 		Self { port_address }
-
-	}
-
-	pub fn write_byte(&self, byte: u8) {
-		let port = self.port_address as *mut u8;
-
-		// LF newline characters need to be extended to CRLF over a real serial port.
-		if byte == b'\n' {
-			unsafe { ptr::write_volatile(port, b'\r'); }
-		}
-
-		unsafe { ptr::write_volatile(port, byte); }
 	}
 }
 
@@ -36,7 +24,7 @@ impl fmt::Write for ComPort {
 		// Output each byte of our string.
 		for &b in s.as_bytes() {
 			// Write our byte.
-			self.write_byte(b);
+			write_byte(self.port_address, b);
 		}
 		Ok(())
 	}
