18c3df9963f05f8f71325c069fb6ee5c5b973b7a
Leonard Rapp <leonard.rapp@rwth-aachen.de>
2019-06-18 21:47:55 +0200

    implement aarch64/serial

--- src/arch/aarch64/serial.rs
+++ src/arch/aarch64/serial.rs
@@ -1,18 +1,31 @@
 use core::fmt;
 use spin::Mutex;
-use aarch64::io::*;
+use core::ptr;
+// use aarch64::io::*;
 
 /// A COM serial port.
 pub struct ComPort {
 	/// COM ports are identified by the base address of their associated
 	/// I/O registers.
-	base_addr: u16
+	port_address: u32
 }
 
 impl ComPort {
 	/// Create a new COM port with the specified base address.
-	const fn new(base_addr: u16) -> ComPort {
-		ComPort { base_addr: base_addr }
+	pub const fn new(port_address: u32) -> Self {
+		Self { port_address }
+
+	}
+
+	pub fn write_byte(&self, byte: u8) {
+		let port = self.port_address as *mut u8;
+
+		// LF newline characters need to be extended to CRLF over a real serial port.
+		if byte == b'\n' {
+			unsafe { ptr::write_volatile(port, b'\r'); }
+		}
+
+		unsafe { ptr::write_volatile(port, byte); }
 	}
 }
 
@@ -20,12 +33,10 @@ impl fmt::Write for ComPort {
 	/// Output a string to our COM port.  This allows using nice,
 	/// high-level tools like Rust's `write!` macro.
 	fn write_str(&mut self, s: &str) -> fmt::Result {
-		unsafe {
-			// Output each byte of our string.
-			for &b in s.as_bytes() {
-				// Write our byte.
-				outb(self.base_addr, b);
-			}
+		// Output each byte of our string.
+		for &b in s.as_bytes() {
+			// Write our byte.
+			self.write_byte(b);
 		}
 		Ok(())
 	}
