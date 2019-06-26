73ce0194a8efc43eb9bac5331110137f550be27f
Leonard Rapp <leonard.rapp@rwth-aachen.de>
2019-06-26 18:14:06 +0200

    add aarch64 to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,9 @@ extern crate spin;
 #[cfg(target_arch = "x86_64")]
 extern crate x86;
 
+#[cfg(target_arch = "aarch64")]
+extern crate aarch64;
+
 // These need to be visible to the linker, so we need to export them.
 pub use logging::*;
 #[cfg(target_arch = "x86_64")]
