e8b119c2f502ebb51328e60c08dfb90b7d907a6d
Leonard Rapp <leonard.rapp@rwth-aachen.de>
2019-07-03 17:08:32 +0200

    wip: init with cortex-a create

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@
 
 #[macro_use]
 extern crate eduos_rs;
+extern crate cortex_a;
 
 use core::panic::PanicInfo;
 use core::ptr;
@@ -14,17 +15,37 @@ use core::ptr;
 
 /// This function is the entry point, since the linker looks for a function
 /// named `_start` by default.
-#[cfg(not(test))]
-#[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> () {
-	unsafe { asm!("ldr %x0, 0x42" :::: "volatile"); }
-	// unsafe { ptr::write_volatile(0x09000000 as *mut u8, 65); }
-	loop{};
-	println!("Hello world!");
-	// loop{};
+//#[cfg(not(test))]
+//#[no_mangle] // don't mangle the name of this function
+//pub extern "C" fn _start() -> ()
+#[link_section = ".text.boot"]
+#[no_mangle]
+pub unsafe extern "C" fn _boot_cores() -> ! {
+	use cortex_a::{asm, regs::*};
 
-	// shutdown system
-	// shutdown();
+	const CORE_0: u64 = 0;
+	const CORE_MASK: u64 = 0x3;
+	const STACK_START: u64 = 0x80_000;
+
+	if CORE_0 == MPIDR_EL1.get() & CORE_MASK {
+		SP.set(STACK_START);
+		unsafe { ptr::write_volatile(0x09000000 as *mut u8, 65); }
+		loop{};
+	} else {
+		// if not core0, infinitely wait for events
+		loop {
+			asm::wfe();
+		}
+	}
+
+// unsafe { asm!("mov x0, #42" :::: "volatile"); }
+// unsafe { ptr::write_volatile(0x09000000 as *mut u8, 65); }
+// loop{};
+// println!("Hello world!");
+// loop{};
+
+// shutdown system
+// shutdown();
 }
 
 /// This function is called on panic.
